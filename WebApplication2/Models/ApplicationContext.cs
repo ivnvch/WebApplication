46c67552c423d8dd64c0445f6663783256a50acc
ivnvch <tarasovets.dmtiv@gmail.com>
2022-07-27 09:16:13 +0300

    master WEB

---
+++ WebApplication2/Models/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Models
+{
+    public class ApplicationContext: DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> dbContextOptions)
+            : base(dbContextOptions)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Company> Companys { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Company>().HasData(new Company { Id = 1, Name = "Тудым-сюдым" });
+        }
+    }
+}
---
+++ WebApplication2/Models/Company.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication2.Models
+{
+    [Table("Companies")]
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public List<User> Users { get; set; } = new List<User>();
+    }
+}
